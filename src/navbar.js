6b8badccef4d1291a1419905ab3b2d5ea15d034f
Kristof924 <B.Kristof2001@gmail.com>
2025-05-28 21:36:21 +0200

    icon

--- src/navbar.js
+++ src/navbar.js
@@ -84,6 +84,7 @@ const Navbar = () => {
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 <meta name="apple-mobile-web-app-title" content="ChatApp" />
+<link rel="apple-touch-icon" href="/icon.png" />
       <nav className="navbar navbar-light bg-light flex-column align-items-start">
         <a className="navbar-brand">{username}</a>
         {showContacts && (
